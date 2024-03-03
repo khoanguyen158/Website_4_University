df10eec49f2d02005d55ac1c89ed07f85c2b3f8c
khoanguyen158 <115911624+khoanguyen158@users.noreply.github.com>
2024-03-03 22:15:08 +0700

    Add files via upload

---
+++ public/otpJa.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var otpInputs = document.querySelectorAll('.otp-input');
+
+    for (var i = 0; i < otpInputs.length; i++) {
+        otpInputs[i].addEventListener('input', function() {
+            if (this.value.length === this.maxLength) {
+                var currentIndex = Array.from(otpInputs).indexOf(this);
+                var nextIndex = currentIndex + 1;
+
+                if (otpInputs[nextIndex]) {
+                    otpInputs[nextIndex].focus();
+                }
+            }
+        });
+    }
+});
