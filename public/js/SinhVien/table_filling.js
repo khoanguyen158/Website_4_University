c0ecfd79cc310405438bd660bb71da3c56cc9627
NguyenDangKhoaDepTrai <khoa.nguyen2211621@hcmut.eud.vn>
2024-04-18 17:05:47 +0700

    yeah yeah

--- public/js/SinhVien/table_filling.js
+++ public/js/SinhVien/table_filling.js
@@ -13,11 +13,6 @@ const firebaseConfig = {
     measurementId: "G-KZQQJNVVWJ"
 };
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// import { getDatabase, set, ref, child, get, onValue, update, remove } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
-// import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -27,15 +22,6 @@ const db = getDatabase(app);
 var stdNo = 0;
 var tbody = document.getElementById('tbody1');
 
-/*function calculateYearlyAverage(process, final, processWeight, finalWeight) {
-    // Tính điểm trung bình cả năm
-    let yearlyAverage = (process * processWeight) + (final * finalWeight);
-
-    // Làm tròn đến 2 chữ số sau dấu phẩy và chuyển chuỗi trở lại thành số
-    yearlyAverage = parseFloat(yearlyAverage.toFixed(2));
-
-    return yearlyAverage;
-}*/
 function formatProcData(proc) {
     let formattedString = '';
     // Duyệt qua từng key trong object proc
@@ -44,14 +30,28 @@ function formatProcData(proc) {
         if (proc.hasOwnProperty(key)) {
             const component = proc[key];
             // Tạo chuỗi với định dạng "Tên: Điểm | "
-            formattedString += `${component.ten}: ${component.diem} | `;
+            formattedString += `${component.ten ||"N/A"}: ${component.diem || "N/A"} | `;
         }
     }
     // Cắt bỏ ký tự '|' cuối cùng khỏi chuỗi
     return formattedString.slice(0, -2);
 }
 
-
+function Status(fi_Re, proc){
+    for (const key in proc) {
+        if (proc.hasOwnProperty(key)) {
+            const component = proc[key];
+            if(!component.ten || !component.diem)
+                return "N/A";
+        }
+    }
+    if(fi_Re == 0)
+         return "Không đạt";
+    else if(fi_Re == "N/A" || !fi_Re)
+        return "N/A";
+    else
+         return "Đạt";
+}
 function AddItemToTable(name, id, proc, fi, cre) {
     let trow = document.createElement("tr");
     let td1 = document.createElement('td');
@@ -65,12 +65,12 @@ function AddItemToTable(name, id, proc, fi, cre) {
 
 
     td1.innerHTML = ++stdNo;
-    td2.innerHTML = name;
-    td3.innerHTML = id;
-    td4.innerHTML = cre;
+    td2.innerHTML = name || "N/A";
+    td3.innerHTML = id || "N/A";
+    td4.innerHTML = cre || "N/A";
     td5.innerHTML = formatProcData(proc);
-    td6.innerHTML = fi;
-    //td7.innerHTML = calculateYearlyAverage(proc, fi, 0.4, 0.6);
+    td6.innerHTML = fi || "N/A";
+    td7.innerHTML = Status(fi, proc);
 
     trow.appendChild(td1);
     trow.appendChild(td2);
@@ -100,37 +100,9 @@ function addSemesterRow(semester) {
     newCell.style.color = "white";
     newCell.style.borderTop = "2px solid black"; // Thêm đường viền phía trên
     newCell.style.borderBottom = "2px solid black"; // Thêm đường viền phía dưới
-    //newCell.style.maxWidth = "50px"; // Thiết lập chiều rộng tối đa cho ô
-    //newCell.style.width = "100%"; // Ô sẽ cố gắng chiếm 100% chiều rộng có sẵn (tối đa 500px)
+
 }
-/*
-const Credit = { Total_Credit: 0, Total_Se_cre: 0,  }; // sô tín chỉ: tổng tích lũy, tích lũy học kỳ
-function Credit_Calc(Cre, totalCre, seCre){
-    if(Cre){
-        totalCre += Cre;
-        seCre += Cre;
-    }
-}
-function AddAllItemsToTable(TheSemester){
-    stdNo = 0;
-    Credit.Total_Credit = 0;
-    Credit.Total_Se_cre = 0;
-    TheSemester.forEach(semester => {
-        addSemesterRow(semester.SeName);
-        Credit.Total_Se_cre = 0;
-        Object.keys(semester).forEach(subjectKey => {
-            if (subjectKey !== "SeName") {
-                let subject = semester[subjectKey];
-                AddItemToTable(subject.NameOfSb, subject.SbID, subject.Process, subject.Final, subject.Cre);
-                Credit_Calc(subject.Cre, Total_Credit, Total_Se_cre);
-            }
-        });
-    });
-    var tt_Cre = document.getElementById('ttCre');
-    tt_Cre.innerText =  Credit.Total_Credit;
-    
-}
-*/
+
 const Credit = { Total_Credit: 0, Total_Se_cre: 0 }; // Số tín chỉ: tổng tích lũy, tích lũy học kỳ
 
 function Credit_Calc(subject) {
@@ -143,24 +115,14 @@ function Credit_Calc(subject) {
 function AddAllItemsToTable(TheSemester) {
     stdNo = 0;
     Credit.Total_Credit = 0; // Khởi tạo lại tổng số tín chỉ
-    /*TheSemester.forEach(element => {
-        AddItemToTable(element.NameOfSb, element.SbID, element.Process, element.Final) // field name source!!
-    });*/
+
     TheSemester.forEach(semester => {
         addSemesterRow(semester.ten_hoc_ki);
         Credit.Total_Se_cre = 0; // Khởi tạo lại số tín chỉ tích lũy cho học kỳ
         stdNo = 0;
         Object.keys(semester).forEach(subjectKey => {
             if (subjectKey !== "ten_hoc_ki") {
-                //alert(subjectKey + " " + semester.val);
                 let subject = semester[subjectKey];
-                // let namex;
-                // get(child(ref(db), 'MonHoc/' + subjectKey)).then((snapshot) => {
-                //     if (snapshot.exists()) {
-                //         //namex = snapshot.val();
-                //         namex = subjectKey;
-                //     }
-                // });
                 AddItemToTable(subject.ten_mon_hoc, subjectKey, subject.so_cot_diem, subject.DTB_10, subject.so_tin_chi);
                 Credit_Calc(subject);
             }
@@ -174,36 +136,12 @@ function AddAllItemsToTable(TheSemester) {
 }
 
 function GetAllDataRealtime() {
-    //const dbRef = ref(db, 'NguyenVanA'); // chưa được hiện thực, chỉ có format, qua onvalue mới truy cập.
-    //const dbRef = ref(db); // tham chiếu đến root
 
-    //---------------- NEED CHANGE!!
-    //var IdInlink = 'SinhVien/NguyenVanA'; // -> need semester ->an outside loop for semes
-    //const dbRef = ref(db, IdInlink);
-    //let UserInfo = JSON.parse(sessionStorage.getItem("user-info"));
-    //let Name = document.getElementById("studentName");
-    //let ID = document.getElementById("studentId");
-    //Name.innerText = `${UserInfo.username}!`;
-    //ID.innerText = `${UserInfo.ID}!`;
-    //StuName.innerText = 'Nguyễn Văn A';
-    //StuID.innerText = "2222";
 
     let UserInfo = JSON.parse(sessionStorage.getItem("user-info"));
-    //var StuName = document.getElementById('studentName');
-    //var StuID = document.getElementById('studentId');
-    //StuName.innerText = UserInfo.ho_va_ten;
-    //StuID.innerText = UserInfo.id;
-    const dbRef = ref(db, 'SinhVien/' + UserInfo.id + '/Hoc_ki');
-    //----------------
 
-    /*get(child(dbRef, "NguyenVanA")) // in database => lấy dữ liệu 1 lần  -> NguyenVanA -> se1{Id,name,...}; se2{Id, name}...
-    .then( (snapshot)=>{
-        var subjects = [];
-        snapshot.forEach(childSnapshot =>{
-            subjects.push(childSnapshot.val());
-        });
-        AddAllItemsToTable(subjects);
-    });*/
+    const dbRef = ref(db, 'SinhVien/' + UserInfo.id + '/Hoc_ki');
+
 
     onValue(dbRef, (snapshot) => {
         var semesters = [];
